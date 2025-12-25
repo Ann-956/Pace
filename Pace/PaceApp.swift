14fdb487db2f7e3103691ecff6fdf1950ca7d8d4
Ann@ <ann-brown956@yandex.ru>
2025-12-24 17:24:58 -0800

    Initial commit

---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by Анна Браун on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
